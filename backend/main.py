fe37eb36555fd1da7c4fcfdf81f7a8c61694097d
Obafemi <adebayokiisi@gmail.com>
2025-12-22 17:42:50 +0100

    added pool_pre_png=True, updated to live sserver from localhost

--- backend/main.py
+++ backend/main.py
@@ -44,7 +44,7 @@ connect_args = {}
 if "sqlite" in DATABASE_URL:
     connect_args = {"check_same_thread": False}
 
-engine = create_engine(DATABASE_URL, connect_args=connect_args)
+engine = create_engine(DATABASE_URL, connect_args=connect_args, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
